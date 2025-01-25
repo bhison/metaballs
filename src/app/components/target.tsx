08c8f507d807b6649310c089c88dcfb3c07afc26
Tim Bhison <catbat04@gmail.com>
2025-01-25 21:22:12 +0000

    target

--- src/app/components/target.tsx
+++ src/app/components/target.tsx
@@ -3,12 +3,12 @@ import { useGame } from "../gameContext";
 import { cn } from "../lib/util";
 
 export const Target = ({ className }: { className?: string }) => {
-  const { target, submitHovered, setSubmitHovered } = useGame();
+  const { target, submitHovered } = useGame();
 
   return (
-    <div className={cn("relative mt-6", className)}>
+    <div className={cn("relative mt-6 z-[-100]", className)}>
       <span
-        className="font-extrabold absolute z-20 text-2xl text-center w-full mt-[-1.85rem]"
+        className="font-extrabold absolute text-2xl text-center w-full mt-[-1.85rem]"
         style={{
           color: "#EEB366",
           textShadow: "2px 2px 1px #FE5937",
@@ -18,11 +18,13 @@ export const Target = ({ className }: { className?: string }) => {
       </span>
       <motion.div
         className={cn(
-          "min-w-[12rem] p-[1%] rounded-[2rem] contain-content flex shadow-lg border-4 border-yellow-300 tansition-color transition-scale cursor-pointer duration-150 opacity-90",
+          "min-w-[12rem] p-[1%] rounded-[2rem] contain-content flex shadow-lg border-4 border-yellow-300 tansition-color transition-scale cursor-pointer duration-150",
           submitHovered ? "bg-green-600 p-[3%]" : "bg-red-500",
           className,
         )}
-        onClick={() => setSubmitHovered(!submitHovered)}
+        onClick={() => {
+          console.log("clicked");
+        }}
         animate={
           submitHovered
             ? {
